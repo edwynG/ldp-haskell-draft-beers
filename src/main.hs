f0b3cabe7aea9ead06072dbd8c4eb9928c8e68f0
Edwyn Guzman <edwynjesus30@gmail.com>
2025-05-24 16:10:14 -0400

    Implemented - addBeer y findBestSolution

--- src/main.hs
+++ src/main.hs
@@ -1,17 +1,59 @@
 module Main where
 
-import Utils (Barrel, validateBarrel, initialBarrels) -- Importamos el modulo Utils externamente como lo declaramos
+import Utils -- Importamos el modulo Utils externamente como lo declaramos
 
--- | Verificamos si es posible servir exactamente n vasos de cerveza desde un barril
-iSolution :: (Barrel, Barrel, Barrel) -> Int -> Bool
-iSolution (barrelA, barrelB, barrelC) n
-    | n < 0     = False
-    | not (validateBarrel barrelA) || not (validateBarrel barrelB) || not (validateBarrel barrelC) = False
-    | otherwise = 
-        let (capA, currA) = barrelA
-            (capB, currB) = barrelB
-            (capC, currC) = barrelC
-        in (capA >= n && currA >= n) || (capB >= n && currB >= n) || (capC >= n && currC >= n)
+-- | Determinar la cantidad de cerveza óptima que debe agregarse en los barriles para poder llenar n vasos desde un barril
+findBestSolution :: Int -> (Barrel, Barrel, Barrel) -> (Int, (Barrel, Barrel, Barrel))
+findBestSolution n (a, b, c)
+    | n <= 0 || not (isSatisfied a b c n) = (0, (a, b, c))
+    | otherwise =
+        let -- Camino agregando a 'a'
+            (addedFromA, stateA) = fromAToC (a, b, c) n 0
+
+            -- Camino agregando a 'c'
+            (addedFromC, stateC) = fromCToA (a, b, c) n 0
+
+            -- Lista de soluciones válidas
+            soluciones = filter (\(ag, est) -> iSolution est n)
+                          [(addedFromA, stateA), (addedFromC, stateC)]
+        in case soluciones of
+            [] -> (0, (a, b, c))
+            xs -> minimum xs -- Elige la de menor cantidad agregada
 
 main :: IO ()
-main = putStrLn ("Hello world!!")
+main = do
+    putStrLn "Barril A: "
+    capA <- getLine
+    putStrLn "contiene"
+    currA <- getLine
+    putStrLn "Barril B: "
+    capB <- getLine
+    putStrLn "contiene"
+    currB <- getLine
+    putStrLn "Barril C: "
+    capC <- getLine
+    putStrLn "contiene"
+    currC <- getLine
+    putStrLn "objetivo:"
+    objetivo <- getLine
+
+    let a = (read capA :: Int, read currA :: Int)
+    let b = (read capB :: Int, read currB :: Int)
+    let c = (read capC :: Int, read currC :: Int)
+    let (barrilA, barrilB, barrilC) = initialBarrels a b c
+    let n = read objetivo :: Int
+
+    let (agregado, (a', b', c')) = findBestSolution n (barrilA, barrilB, barrilC)
+
+    if  iSolution (a', b', c') n
+        then
+            if (fst a' >= n && snd a' >= n)
+                then putStrLn $ "agregar " ++ show agregado ++ "L de cerveza desde el Barril A para servir los " ++ show n ++ " vasos de cerveza desde el"
+            else if (fst c' >= n && snd c' >= n)
+                then putStrLn $ "agregar " ++ show agregado ++ "L de cerveza desde el Barril C para servir los " ++ show n ++ " vasos de cerveza desde el"
+            else if (snd a' >= fst a')
+                then putStrLn $ "agregar " ++ show agregado ++ "L de cerveza desde el Barril A para servir los " ++ show n ++ " vasos de cerveza desde el Barril B"
+            else
+                putStrLn $ "agregar " ++ show agregado ++ "L de cerveza desde el Barril C para servir los " ++ show n ++ " vasos de cerveza desde el Barril B"
+        else
+            putStrLn "No es posible servir la cantidad deseada."
